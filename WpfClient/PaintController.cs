6f5071f929e137dba541a420eb99fa40bb98dd98
stasvch <yana08469@gmail.com>
2025-11-13 23:21:59 +0300

    create project

---
+++ WpfClient/PaintController.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO.Ports;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Threading;
+
+namespace WpfClient
+{
+    public sealed class PaintController : IDisposable
+    {
+        private readonly PaintViewModel _viewModel;
+        private readonly DispatcherTimer _timer;
+        private readonly SerialPort? _serialPort;
+        private bool _isRunning;
+
+        private static readonly Dictionary<string, Action<PaintViewModel, string[]>> SerialCommands = new()
+        {
+            ["CURSOR"] = (vm, args) =>
+            {
+                if (args.Length >= 2 &&
+                    double.TryParse(args[0], NumberStyles.Float, CultureInfo.InvariantCulture, out var x) &&
+                    double.TryParse(args[1], NumberStyles.Float, CultureInfo.InvariantCulture, out var y))
+                {
+                    vm.CursorX = x;
+                    vm.CursorY = y;
+                }
+            },
+            ["COLOR"] = (vm, args) =>
+            {
+                if (args.Length >= 1)
+                {
+                    vm.SelectedColor = args[0];
+                }
+            },
+            ["FIGURE"] = (vm, args) =>
+            {
+                if (args.Length >= 1)
+                {
+                    vm.FilledFigures.Add(string.Join(' ', args));
+                }
+            },
+            ["PICTURE"] = (vm, args) =>
+            {
+                if (args.Length >= 1)
+                {
+                    vm.PictureType = args[0];
+                }
+            }
+        };
+
+        public PaintController(PaintViewModel viewModel, DispatcherTimer timer, SerialPort? serialPort)
+        {
+            _viewModel = viewModel;
+            _timer = timer;
+            _serialPort = serialPort;
+        }
+
+        public void Run()
+        {
+            if (_isRunning)
+            {
+                return;
+            }
+
+            _timer.Tick += OnTick;
+            _timer.Start();
+            CompositionTarget.Rendering += OnRendering;
+
+            if (_serialPort is not null)
+            {
+                _serialPort.DataReceived += OnSerialDataReceived;
+                try
+                {
+                    if (!_serialPort.IsOpen)
+                    {
+                        _serialPort.Open();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    System.Diagnostics.Debug.WriteLine($"Failed to open serial port: {ex.Message}");
+                }
+            }
+
+            _isRunning = true;
+        }
+
+        public void Stop()
+        {
+            if (!_isRunning)
+            {
+                return;
+            }
+
+            _timer.Tick -= OnTick;
+            _timer.Stop();
+            CompositionTarget.Rendering -= OnRendering;
+
+            if (_serialPort is not null)
+            {
+                _serialPort.DataReceived -= OnSerialDataReceived;
+                if (_serialPort.IsOpen)
+                {
+                    _serialPort.Close();
+                }
+            }
+
+            _isRunning = false;
+        }
+
+        private void OnTick(object? sender, EventArgs e)
+        {
+            _viewModel.LastUpdate = DateTimeOffset.Now;
+        }
+
+        private void OnRendering(object? sender, EventArgs e)
+        {
+            _viewModel.LastRender = DateTimeOffset.Now;
+        }
+
+        private void OnSerialDataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            try
+            {
+                var line = _serialPort?.ReadLine();
+                if (string.IsNullOrWhiteSpace(line))
+                {
+                    return;
+                }
+
+                ProcessSerialMessage(line);
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine($"Failed to read serial data: {ex.Message}");
+            }
+        }
+
+        private void ProcessSerialMessage(string message)
+        {
+            var parts = message.Trim().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            if (parts.Length == 0)
+            {
+                return;
+            }
+
+            var command = parts[0].ToUpperInvariant();
+            var args = parts.Length > 1 ? parts[1..] : Array.Empty<string>();
+
+            if (SerialCommands.TryGetValue(command, out var handler))
+            {
+                Application.Current.Dispatcher.Invoke(() => handler(_viewModel, args));
+            }
+        }
+
+        public void Dispose()
+        {
+            Stop();
+            _serialPort?.Dispose();
+        }
+    }
+}
---
+++ WpfClient/PaintViewModel.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfClient
+{
+    public class PaintViewModel : INotifyPropertyChanged
+    {
+        private string _pictureType = "None";
+        private ObservableCollection<string> _filledFigures = new();
+        private double _cursorX;
+        private double _cursorY;
+        private string _selectedColor = "Black";
+        private DateTimeOffset _lastUpdate = DateTimeOffset.MinValue;
+        private DateTimeOffset _lastRender = DateTimeOffset.MinValue;
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public string PictureType
+        {
+            get => _pictureType;
+            set => SetField(ref _pictureType, value);
+        }
+
+        public ObservableCollection<string> FilledFigures
+        {
+            get => _filledFigures;
+            set => SetField(ref _filledFigures, value);
+        }
+
+        public double CursorX
+        {
+            get => _cursorX;
+            set => SetField(ref _cursorX, value);
+        }
+
+        public double CursorY
+        {
+            get => _cursorY;
+            set => SetField(ref _cursorY, value);
+        }
+
+        public string SelectedColor
+        {
+            get => _selectedColor;
+            set => SetField(ref _selectedColor, value);
+        }
+
+        public DateTimeOffset LastUpdate
+        {
+            get => _lastUpdate;
+            set => SetField(ref _lastUpdate, value);
+        }
+
+        public DateTimeOffset LastRender
+        {
+            get => _lastRender;
+            set => SetField(ref _lastRender, value);
+        }
+
+        protected void SetField<T>(ref T field, T value, [CallerMemberName] string? propertyName = null)
+        {
+            if (EqualityComparer<T>.Default.Equals(field, value))
+            {
+                return;
+            }
+
+            field = value;
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
